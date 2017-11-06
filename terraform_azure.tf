c9ae1e370f3a0a9934e497bb708b3e4172e11dc6
Jose Yapur <joseyapur@Joses-MacBook-Air.local>
2017-11-06 11:06:18 -0500

    Codigo Inicial

---
+++ terraform_azure.tf
@@ -0,0 +1,28 @@
+resource "azurerm_resource_group" "aci-rg" {
+  name     = "RGDemoBanco"
+  location = "westus"
+}
+
+resource "azurerm_container_group" "aci-mibanco" {
+  name                = "aci-mibanco"
+  location            = "${azurerm_resource_group.aci-rg.location}"
+  resource_group_name = "${azurerm_resource_group.aci-rg.name}"
+  ip_address_type     = "public"
+  os_type             = "linux"
+
+  container {
+    name   = "nginx"
+    image  = "nginx"
+    cpu    = "2"
+    memory = "3.5"
+    port   = "80"
+
+    environment_variables {
+      "NODE_ENV" = "Staging"
+    }
+  }
+
+  tags {
+    environment = "testing"
+  }
+} 
